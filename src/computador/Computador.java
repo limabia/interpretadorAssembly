2c06e05657ac06513d7992fa2dfee6b7f9c991c3
Marcelo <marcelo-torres>
2018-06-06 13:59:16 -0300

    "Adicionado o metodo main"

--- src/computador/Computador.java
+++ src/computador/Computador.java
@@ -1,5 +1,7 @@
 package computador;
 
+import computador.componentes.ULA;
+
 /**
  *
  * Data: 31-05-2018
@@ -8,4 +10,7 @@ public class Computador {
     
     public static final int TAMANHO_PALAVRA = 16;
     
+    public static void main(String[] args) {
+  
+    }
 }
