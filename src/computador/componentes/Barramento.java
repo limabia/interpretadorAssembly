232e8a4d02d96db80d5442f8af16e9a313bcbca5
a10414571 <a10414571@LAB07M15P017221.uspeachsi.uspnet>
2018-06-13 17:09:03 -0300

    A classe foi modificada para salvar todas as conexoes que envolvem elas em duas listas sepradas: as de entrada e as de saida.

--- src/computador/componentes/Barramento.java
+++ src/computador/componentes/Barramento.java
@@ -3,8 +3,11 @@ package computador.componentes;
 import computador.conexao.Conectavel;
 import computador.conexao.ConexaoBinaria;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 /**
- * Classe que representa um barramento atraves de um variavel do tipo vetor
+ * Classe que representa um barramento atraves de uma variavel do tipo vetor
  * para inteiro.
  * 
  * Data: 31-05-2018
@@ -13,6 +16,10 @@ public class Barramento implements Conectavel {
     
     // Variavel que representa o valor armazenado no barramento
     private int[] binario;
+    
+    // Conjuntos de conexoes envolvendo o Barramento
+    private ArrayList<ConexaoBinaria> entradas; //Conexoes no qual o Barramento eh o destino
+    private ArrayList<ConexaoBinaria> saidas; // Conexoes no qual o Barramento eh a origem
     
     
     
@@ -23,9 +30,38 @@ public class Barramento implements Conectavel {
      */
     public Barramento(int tamanho) {
         this.binario = new int[tamanho];
-    }
 
+        this.entradas = new ArrayList<ConexaoBinaria>();        
+        this.saidas = new ArrayList<ConexaoBinaria>();
+    }
     
+    
+    
+    /**
+     * Ativa o enviao de dados comecando pelas conexoes abertas que escrevem no
+     * barramento e terminando pelas conexoes abertas que leem do barramento.
+     * Apos o metodo 'conectar' de cada conexao ser chamado, o metodo 'fechar'
+     * da mesma conexao eh chamdo para fechar a conexao.
+     */
+    public void conectar() {
+        Iterator<ConexaoBinaria> conexoes;
+        ConexaoBinaria conexao;
+        
+        // Ativa as conexoes que escrevem no barramento
+        conexoes = this.entradas.iterator();
+        while(conexoes.hasNext()) {
+            conexao = conexoes.next();
+            conexao.conectar();
+            conexao.fechar();
+        }
+        // Ativa as conexoes de leem do barramento
+        conexoes = this.saidas.iterator();
+        while(conexoes.hasNext()) {
+            conexao = conexoes.next();
+            conexao.conectar();
+            conexao.fechar();
+        }
+    }
     
     /**
      * Define o Barramento como destino de um objeto ConexaoBinaria.
@@ -36,6 +72,7 @@ public class Barramento implements Conectavel {
      */
     @Override
     public boolean definirComoDestino(ConexaoBinaria conexao) {
+        this.entradas.add(conexao);
         return conexao.definirDestino(this.binario);
     }
     
@@ -48,6 +85,7 @@ public class Barramento implements Conectavel {
      */
     @Override
     public boolean definirComoOrigem(ConexaoBinaria conexao) {
+        this.saidas.add(conexao);
         return conexao.definirOrigem(this.binario);
     }
     
